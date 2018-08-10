3bb52e2a89f49a9ddd6e1314e0dc3ce37fd6c1c5
linson <linson@linsondeMac.local>
2018-08-10 08:48:59 +0800

    Initial Commit

---
+++ OurCity100/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OurCity100
+//
+//  Created by linson on 2018/8/10.
+//  Copyright © 2018年 linson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ OurCity100/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  OurCity100
+//
+//  Created by linson on 2018/8/10.
+//  Copyright © 2018年 linson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
